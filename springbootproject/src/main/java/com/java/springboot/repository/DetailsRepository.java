7e3602c1ec39ce4f0c53bb7442079b326245be2c
R.J.PRAVEEN <97513071+Praveenrj17@users.noreply.github.com>
2022-03-25 12:35:07 +0530

    Delete springbootproject directory

--- springbootproject/src/main/java/com/java/springboot/repository/DetailsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.java.springboot.repository;
-import org.springframework.data.repository.CrudRepository;
-import com.java.springboot.details.Details;
-
-	public interface DetailsRepository extends CrudRepository<Details,String> 
-	{		
-		
-	}
